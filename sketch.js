2ea02184d42545c7156fa606dd7b51928420f868
taynara <hasckel.taynara@escola.pr.gov.br>
2024-08-09 09:42:42 -0300

    atualizações

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+    
+    background("pink");
+  }
+  
+  function draw() {
+    
+    rect (30,20,20);
+    fill("white");
+    stroke("black");  
+  if (mouseIsPressed);
+    {
+    rect(mouseX,mouseY,20,20);}
+    
+  }
+  
